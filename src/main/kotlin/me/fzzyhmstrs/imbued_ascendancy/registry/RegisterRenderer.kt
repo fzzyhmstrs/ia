b54afa827f95fbba3c2c30c7af5f9e2cd7760387
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-04-21 10:37:26 -0400

    Update RegisterRenderer.kt

--- src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/registry/RegisterRenderer.kt
+++ src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/registry/RegisterRenderer.kt
@@ -2,9 +2,10 @@
 
 package me.fzzyhmstrs.imbued_ascendancy.registry
 
+import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
 import me.fzzyhmstrs.imbued_ascendancy.model.CelestialTridentEntityModel
 import me.fzzyhmstrs.imbued_ascendancy.model.CelestialTridentEntityRenderer
-import me.fzzyhmstrs.amethyst_imbuement.model.GlisteringTridentEntityRenderer
 import me.fzzyhmstrs.imbued_ascendancy.IA
 import me.fzzyhmstrs.imbued_ascendancy.model.ChampionsTridentEntityModel
 import me.fzzyhmstrs.imbued_ascendancy.model.ChampionsTridentEntityRenderer
@@ -94,6 +95,22 @@ object RegisterRenderer {
         ) { stack: ItemStack, _: ClientWorld?, entity: LivingEntity?, _: Int ->
             val nbt = stack.nbt?:return@register 0.0f
             if (entity != null && nbt.getInt("tier") > 1) 1.0f else 0.0f
+        }
+        
+        ModelPredicateProviderRegistry.register(
+            RegisterItem.BOOK_OF_LEGEND, Identifier("type")
+        ) { stack: ItemStack, _: ClientWorld?, _: LivingEntity?, _: Int ->
+            val nbt = stack.orCreateNbt
+            if (nbt.contains(NbtKeys.LORE_TYPE.str())){
+                when(nbt.getString(NbtKeys.LORE_TYPE.str())){
+                    SpellType.FURY.str() ->{ 0.3f }
+                    SpellType.GRACE.str() -> { 0.7f }
+                    SpellType.WIT.str() -> { 1.0f }
+                    else -> { 0.0f }
+                }
+            } else {
+                0.0f
+            }
         }
     }
 }
