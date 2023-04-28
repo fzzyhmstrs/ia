e0e8ae2908f65e8f2f6351d5281092b4ab7e64ca
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-04-28 11:47:24 -0400

    Update ModifierFunctions.kt

--- src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/modifier/ModifierFunctions.kt
+++ src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/modifier/ModifierFunctions.kt
@@ -2,6 +2,8 @@ package me.fzzyhmstrs.imbued_ascendancy.modifier
 
 import me.fzzyhmstrs.gear_core.modifier_util.EquipmentModifier
 import me.fzzyhmstrs.imbued_ascendancy.config.IaConfig
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.player.PlayerEntity
 
 object ModifierFunctions {
 
@@ -34,8 +36,10 @@ object ModifierFunctions {
         }
 
     val WARRIORS_LIGHT_DAMAGE_FUNCTION: EquipmentModifier.DamageFunction =
-        EquipmentModifier.DamageFunction { _, _, attacker, damage, amount ->
+        EquipmentModifier.DamageFunction { _, user, attacker, damage, amount ->
             if (attacker != null && attacker.isUndead){
+                val source = if(user is PlayerEntity) DamageSource.player(user) else DamageSource.mob(user)
+                attacker.damage(source, IaConfig.modifiers.warriorsLightDamage.get())
                 amount * IaConfig.modifiers.warriorsLightMultiplier.get()
             } else {
                 amount
