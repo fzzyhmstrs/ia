11523c25b0893e8d0a2c4840483a59c0fef26cc9
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-04-28 11:44:09 -0400

    Update ModifierFunctions.kt

--- src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/modifier/ModifierFunctions.kt
+++ src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/modifier/ModifierFunctions.kt
@@ -15,27 +15,28 @@ object ModifierFunctions {
         }
 
     val MANA_REACTIVE_DAMAGE_FUNCTION: EquipmentModifier.DamageFunction =
-        EquipmentModifier.DamageFunction { _, _, _, damage, amount ->
+        EquipmentModifier.DamageFunction { _, user, _, damage, amount ->
             if (damage.isMagic){
-                amount * IaConfig.modifiers.voidShroudedMultiplier.get()
+                ModifierConsumers.manaHealItems(user,IaConfig.modifiers.gear.manaReactiveAmount.get() * 2)
             } else {
-                amount
+                ModifierConsumers.manaHealItems(user,IaConfig.modifiers.gear.manaReactiveAmount.get())
             }
+            amount
         }
 
     val HELIOPHOBIA_DAMAGE_FUNCTION: EquipmentModifier.DamageFunction =
-        EquipmentModifier.DamageFunction { _, _, _, damage, amount ->
-            if (damage.isMagic){
-                amount * IaConfig.modifiers.voidShroudedMultiplier.get()
+        EquipmentModifier.DamageFunction { _, user, _, damage, amount ->
+            if (user.world.isSkyVisible(user.blockPos) && world.isDay){
+                amount * IaConfig.modifiers.heliophobiaMultiplier.get()
             } else {
                 amount
             }
         }
 
     val WARRIORS_LIGHT_DAMAGE_FUNCTION: EquipmentModifier.DamageFunction =
-        EquipmentModifier.DamageFunction { _, _, _, damage, amount ->
-            if (damage.isMagic){
-                amount * IaConfig.modifiers.voidShroudedMultiplier.get()
+        EquipmentModifier.DamageFunction { _, _, attacker, damage, amount ->
+            if (attacker != null && attacker.isUndead){
+                amount * IaConfig.modifiers.warriorsLightMultiplier.get()
             } else {
                 amount
             }
