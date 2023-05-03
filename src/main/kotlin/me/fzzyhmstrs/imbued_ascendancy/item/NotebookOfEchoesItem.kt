6f18396cf11cf98fe23dcb110950d335165079b4
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-03 14:05:25 -0400

    Update NotebookOfEchoesItem.kt

--- src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/item/NotebookOfEchoesItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/imbued_ascendancy/item/NotebookOfEchoesItem.kt
@@ -1,6 +1,25 @@
 package me.fzzyhmstrs.imbued_ascendancy.item
 
 import net.minecraft.item.Item
+import net.minecraft.client.MinecraftClient
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.Hand
+import net.minecraft.util.Identifier
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
 
 class NotebookOfEchoesItem(settings: Settings): Item(settings) {
+  
+  override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+      val stack = user.getStackInHand(hand)
+      if (world.isClient){
+          world.playSound(user,user.blockPos,SoundEvents.ITEM_BOOK_PAGE_TURN,SoundCategory.PLAYER,0.7f,1.0f)
+          MinecraftClient.getInstance().setScreen(null)
+      }
+      return super.use(world,user,hand)
+  }
+  
 }
